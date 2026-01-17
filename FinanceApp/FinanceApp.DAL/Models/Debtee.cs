f838041516ba4f2272f76d32b88e1eae29789605
katemcasto <Katemcasto@outlook.com>
2026-01-17 18:36:30 -0500

    Added button and field to add debtee on frontend

--- FinanceApp/FinanceApp.DAL/Models/Debtee.cs
+++ FinanceApp/FinanceApp.DAL/Models/Debtee.cs
@@ -3,9 +3,11 @@
     public class Debtee
     {
         public required string DebteeName { get; set; }
-        public string? ContactName { get; set; }
+        public string? ContactFirstName { get; set; }
+        public string? ContactLastName { get; set; }
         public string? ContactNumber { get; set; }
         public string? BillingAddress { get; set; }
+        public string? BillingCity { get; set; }
         public string? BillingState { get; set; }
         public string? BillingZipCode { get; set; }
     }
--- FinanceApp/src/App.tsx
+++ FinanceApp/src/App.tsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+type Debtee = {
+  id: number;
+  debteeName: string;
+  contactFirstName: string;
+  contactLastName: string;
+  contactNumber: string;
+  billingAddress: string;
+  billingCity: string;
+  billingState: string;
+  billingZipCode: number;
+}
+ 
 function App() {
-  const [count, setCount] = useState(0)
+  const [debtees, setDebtees] = useState<Debtee[]>([]);
+  const [debteeName, setDebteeName] = useState(''); 
+  const [contactFirstName, setContactFirstName] = useState(''); 
+  const [contactLastName, setContactLastName] = useState(''); 
+  const [contactNumber, setContactNumber] = useState(''); 
+  const [billingAddress, setBillingAddress] = useState(''); 
+  const [billingCity, setBillingCity] = useState(''); 
+  const [billingState, setBillingState] = useState(''); 
+  const [billingZipCode, setBillingZipCode] = useState(0); 
+
+  const addDebtee = (newDebtee : Debtee) => {
+    setDebtees(prevDebtees => [...prevDebtees, newDebtee]);
+  };
+
+  const handleAddDebtee = () => {
+    addDebtee({
+      id: 1,
+      debteeName: debteeName,
+      contactFirstName: contactFirstName,
+      contactLastName: contactLastName,
+      contactNumber: contactNumber,
+      billingAddress: billingAddress,
+      billingCity: billingCity,
+      billingState: billingState,
+      billingZipCode: billingZipCode
+    });
+    setDebteeName('');
+    setContactFirstName('');
+    setContactLastName('');
+    setContactNumber('');
+    setBillingAddress('');
+    setBillingCity('');
+    setBillingState('');
+    setBillingZipCode(0);
+  };
+
+
 
   return (
     <>
+      <input 
+        type='text' 
+        placeholder='Debtee Name' 
+        value={debteeName}
+        onChange={e => setDebteeName(e.target.value)}
+      />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={handleAddDebtee}>Add Debtee</button>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {debtees.map(debtee => (
+          <li key={debtee.id}>
+            {debtee.debteeName}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
